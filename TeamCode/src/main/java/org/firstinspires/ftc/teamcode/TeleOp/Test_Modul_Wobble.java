36f049d1f267805308f6dcebc4885ac9b0ff00aa
SkyBang <55296576+SkyBang@users.noreply.github.com>
2020-12-22 16:59:34 +0200

    ctrl+alt+L e epic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Test_Modul_Wobble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Test_Modul_Wobble.java
@@ -9,27 +9,28 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name= "TestModulWobble")
+@TeleOp(name = "TestModulWobble")
 public class Test_Modul_Wobble extends LinearOpMode {
 
     private Boolean CrSensLastDep = true;
+    Servo clamp = hardwareMap.get(Servo.class, "SR_CLAMP");
+    CRServo cremaliera_Servo = hardwareMap.get(CRServo.class, "SR_CRM");
+    DigitalChannel digChannel = hardwareMap.get(DigitalChannel.class, "Mag_CRM");
+    boolean crMg_OK;
 
     /**
      *
      */
     @Override
     public void runOpMode() {
+        /**public DcMotor MotorDF = null;
+         //public DcMotor MotorSF = null;
+         //public DcMotor MotorDJ = null;
+         //public DcMotor MotorSJ = null;
+         //private DcMotor ColectorE = null;
+         private DcMotor ColectorV = null;**/
 
 
-        //public DcMotor MotorDF = null;
-        //public DcMotor MotorSF = null;
-        //public DcMotor MotorDJ = null;
-        //public DcMotor MotorSJ = null;
-        //private DcMotor ColectorE = null;
-        //private DcMotor ColectorV = null;
-        CRServo cremaliera_Servo = hardwareMap.get(CRServo.class, "SR_CRM");
-        DigitalChannel digChannel = hardwareMap.get(DigitalChannel.class, "Mag_CRM");
-        Servo clamp=hardwareMap.get(Servo.class, "SR_CLAMP");
         // set the digital channel to input.
         digChannel.setMode(DigitalChannel.Mode.INPUT);
 
@@ -42,155 +43,76 @@ public class Test_Modul_Wobble extends LinearOpMode {
 
 
             // run until the end of the match (driver presses STOP)
+            ClampTeleOp();
 
 
-                // check to see if we need to move the servo.
-                if(gamepad1.y) {
-                    // move to 0 degrees.
-                    clamp.setPosition(0.0);
+        }
+    }
 
-                } else if (gamepad1.x) {
-                    // move to 90 degrees.
-                    clamp.setPosition(0.5);
+    // run until the end of the match (driver presses STOP)
+    public void ClampTeleOp() {
+
+        if (gamepad1.y) {
+            // move to 0 degrees.
+            clamp.setPosition(0.1);
+
+        } else if (gamepad1.x) {
+            // move to 90 degrees.
+            clamp.setPosition(0.5);
+        }
+        telemetry.addData("Servo Position", clamp.getPosition());
+        telemetry.addData("Status", "Running");
+    }
+
+    public void CremalieraTeleOp() {
+        crMg_OK = !digChannel.getState();
+        if (CrSensLastDep) {
+            if (crMg_OK) {
+                if (gamepad1.a) {
+                    cremaliera_Servo.setPower(0);
+                    //CrSensLastDep = false;
+                } else if (gamepad1.b) {
+                    cremaliera_Servo.setPower(-1);
+                    //CrSensLastDep = false;
                 }
-                telemetry.addData("Servo Position", clamp.getPosition());
-                telemetry.addData("Status", "Running");
 
-
-
-
-
-            boolean crMg_OK;
-            /*if (CrSensLastDep == false) {
-                    CrInt_OK = false;
-                    CrExt_OK = true;
-                } else if (CrSensLastDep == true) {
-                    CrExt_OK = false;
-                    CrInt_OK = true;
-                }*/
-            /* if (CrPw_0 == false) {
-                    Cremaliera_Servo.setPower(0);
-                    CrPw_0 = true;
-                }*/
-            crMg_OK = !digChannel.getState();
-
-
-                // Buton A pentru cremaliera in exterior = True
-                // Buton B pentru cremaliera in interior = False
-                // CrSensDep = 0 deplaseaza cremaliera in interior
-                // CrSensDep = 1 deplaseaza cremaliera in exterior
-                /*if (gamepad1.b) {
-                    if (CrSensLastDep == false ) {
-                        if (CrMg_OK == false) {
-                            Cremaliera_Servo.setPower(-1);
-                            CrSensLastDep = false;
-                        }
-
-                    }
-                        else if (CrSensLastDep == true)
-                        {
-                            if (CrMg_OK == true) {
-                                Cremaliera_Servo.setPower(0);
-                                CrSensLastDep = false;
-                            }
-                        }
-                    }
-                    // move to 90 degrees.
-                    /*if (CrInt_OK == true) {
-                        Cremaliera_Servo.setPower(-1);
-                        CrSensDep = false;
-                        CrPw_0 = false;
-                    }
-                 else if (gamepad1.a) {
-                    if (CrSensLastDep == true )
-                    {
-                        if (CrMg_OK == false)
-                        {
-                            Cremaliera_Servo.setPower(-1);
-                            CrSensLastDep = true;
-                        }
-                    }
-                    // move to 180 degrees.
-                    if (CrExt_OK == true) {
-                        Cremaliera_Servo.setPower(1);
-                        CrSensDep = true;
-                        CrPw_0 = false;
-                    }
-            } else if (gamepad1.y) {
-                    // move to 180 degrees.
-                    Cremaliera_Servo.setPower(0);
-                }*/
-                // 1100-0
-                // 0101-0
-                //0100-0
-
-                if (CrSensLastDep)
-                {   if (crMg_OK)
-                    {
-                        if (gamepad1.a)
-                        {
-                            cremaliera_Servo.setPower(0);
-                            //CrSensLastDep = false;
-                        }
-                        else if (gamepad1.b)
-                        {
-                            cremaliera_Servo.setPower(-1);
-                            //CrSensLastDep = false;
-                        }
-
-                    }
-                    else {
-                        if (gamepad1.a)
-                        {
-                            cremaliera_Servo.setPower(1);
-                            ////////////////////////CrSensLastDep = true;
-                        }
-                        else if(gamepad1.b){
-                            cremaliera_Servo.setPower(0);
-                            CrSensLastDep =false;
-                        }
-                        else{
-                            cremaliera_Servo.setPower(0);
-                        }
-                    }
+            } else {
+                if (gamepad1.a) {
+                    cremaliera_Servo.setPower(1);
+                    ////////////////////////CrSensLastDep = true;
+                } else if (gamepad1.b) {
+                    cremaliera_Servo.setPower(0);
+                    CrSensLastDep = false;
+                } else {
+                    cremaliera_Servo.setPower(0);
                 }
-                else {
-                    if (crMg_OK) {
-                        if (gamepad1.b)
-                        {
-                            cremaliera_Servo.setPower(0);
-                        }
-                        else if (gamepad1.a)
-                        {
-                            cremaliera_Servo.setPower(1);
-                            //CrSensLastDep = true;
-                        }
-                    }
-                    if (!crMg_OK) {
-                        if (gamepad1.b)
-                        {
-                            cremaliera_Servo.setPower(-1);
-                        }
-                        else if (gamepad1.a){
-                            cremaliera_Servo.setPower(1);
-                            CrSensLastDep = true;
-                        }
-                        else {
-                            cremaliera_Servo.setPower(0);
-                        }
-                    }
+            }
+        } else {
+            if (crMg_OK) {
+                if (gamepad1.b) {
+                    cremaliera_Servo.setPower(0);
+                } else if (gamepad1.a) {
+                    cremaliera_Servo.setPower(1);
+                    //CrSensLastDep = true;
                 }
-            telemetry.addData("sens", CrSensLastDep);
-            telemetry.addData("magnet",crMg_OK);
-            telemetry.addData("Buton A", gamepad1.a);
-            telemetry.addData("Buton B", gamepad1.b);
-            telemetry.update();
-
-
+            }
+            if (!crMg_OK) {
+                if (gamepad1.b) {
+                    cremaliera_Servo.setPower(-1);
+                } else if (gamepad1.a) {
+                    cremaliera_Servo.setPower(1);
+                    CrSensLastDep = true;
+                } else {
+                    cremaliera_Servo.setPower(0);
+                }
             }
         }
-        // run until the end of the match (driver presses STOP)
-
-
+        telemetry.addData("sens", CrSensLastDep);
+        telemetry.addData("magnet", crMg_OK);
+        telemetry.addData("Buton A", gamepad1.a);
+        telemetry.addData("Buton B", gamepad1.b);
+        telemetry.update();
     }
+
+}
 
